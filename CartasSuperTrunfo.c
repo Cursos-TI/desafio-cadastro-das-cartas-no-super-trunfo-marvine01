37415548c68f7717c80f9dfc6b3a38c857e86054
marvine01 <marvine01@gmail.com>
2025-08-28 14:36:59 +0000

    alterações

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,5 +3,30 @@
 int main() {
     printf("Desafio Cadastro de cartas no super trunfo!\n");
     printf("Novo commit\n");
+    int num1
+    char letra a
+    int Populaçao 1 = 25;        // Declara População e atribui um valor 25
+    float área 1 = 80.000;           // Declara "área" e atribui um valor 80.000
+    float PIB 1 = 80.000;           // Declara "PIB" e atribui um valor 80.000
+    int Populaçao 1 = 25;        // Declara População e atribui um valor 25
+
+    int Populaçao 2 = 25;        // Declara População e atribui um valor 25
+    float área 2 = 80.000;           // Declara "área" e atribui um valor 80.000
+    float PIB 2 = 80.000;           // Declara "PIB" e atribui um valor 80.000
+    int Populaçao 2 = 25;        // Declara População e atribui um valor 25
+
+    int Populaçao 3 = 25;        // Declara População e atribui um valor 25
+    float área 3 = 80.000;           // Declara "área" e atribui um valor 80.000
+    float PIB 3 = 80.000;           // Declara "PIB" e atribui um valor 80.000
+    int Populaçao 3 = 25;        // Declara População e atribui um valor 25
+
+     int Populaçao 4 = 25;        // Declara População e atribui um valor 25
+    float área 4 = 80.000;           // Declara "área" e atribui um valor 80.000
+    float PIB 4 = 80.000;           // Declara "PIB" e atribui um valor 80.000
+    int Populaçao 4 = 25;        // Declara População e atribui um valor 25
+    
+    //Lendo um numero e uma letra do usuario para formar a carta
+   
+    scanf("Digite a primeira letra da carta")
 }
     
